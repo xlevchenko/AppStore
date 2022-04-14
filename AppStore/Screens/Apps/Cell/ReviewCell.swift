30173bba7d9759f7df417bb8a63d58561275f5a4
Olexsii <realcarter50@gmail.com>
2022-04-14 20:50:01 +0300

    App Reviews Controller

--- AppStore/Screens/Apps/Cell/ReviewCell.swift
+++ AppStore/Screens/Apps/Cell/ReviewCell.swift
@@ -9,10 +9,10 @@ import UIKit
 
 class ReviewCell: UICollectionViewCell {
     
-    let titleLabel = TitleLable(text: "Review & Reting", textAlignment: .left, fontSize: 16)
-    let authorLabel = TitleLable(text: "Olexsi Levchenko", textAlignment: .left, fontSize: 16)
+    let titleLabel = TitleLable(text: "Review & Reting", textAlignment: .left, fontSize: 18)
+    let authorLabel = SecondaryTitleLable(text: "Olexsi Levchenko", fontSize: 16)
     let starLabel = TitleLable(text: "Stars", textAlignment: .left, fontSize: 14)
-    let bodyLabel = TitleLable(text: "Body Label\nBody Label\nBody Label\nBody Label\n", textAlignment: .left, fontSize: 14)
+    let bodyLabel = TitleLable(text: "Body Label\nBody Label\nBody Label\nBody Label\n", textAlignment: .left, fontSize: 16)
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -30,5 +30,15 @@ class ReviewCell: UICollectionViewCell {
     func configure() {
         authorLabel.font = UIFont.systemFont(ofSize: 16)
         starLabel.font = UIFont.systemFont(ofSize: 14)
+        bodyLabel.font = UIFont.systemFont(ofSize: 14)
+        bodyLabel.numberOfLines = 0
+        
+        let stackView = VerticalStackView(arrangedSubviews: [
+            UIStackView(arrangedSubviews: [titleLabel, UIView(), authorLabel]),
+            starLabel,
+            bodyLabel
+        ], spacing: 12)
+        addSubview(stackView)
+        stackView.fillSuperview(padding: .init(top: 20, left: 20, bottom: 20, right: 20 ))
     }
 }
